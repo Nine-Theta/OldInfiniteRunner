5aac3c540126692bae509ad31c9ab472632292c3
DESKTOP-J3DPF1E\Ollie <muraticum@gmail.com>
2017-11-27 12:12:27 +0100

    Particle effects and slimes

---
+++ 2D test box/Assets/Scripts/CordTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CordTest : MonoBehaviour {
+
+    public GameObject cordPart;
+    public GameObject player;
+
+    public int cordLength = 500;
+    public float cordFloppiness = 1; //Floppiness™
+
+    private GameObject[] _cordParts;
+    private float _sineLengthModifier = 0.002f;
+    
+	private void Start () {
+        _cordParts = new GameObject[cordLength];
+
+        for (int i = 0; i < cordLength; i++)
+        {
+            GameObject newCord = Instantiate(cordPart);
+            _cordParts[i] = newCord;
+        }
+        _sineLengthModifier = 1.0f/cordLength;
+    }
+	
+	private void Update () {
+        if((this.gameObject.transform.position - player.transform.position).magnitude / cordLength > 0.25f){
+            player.transform.position = this.gameObject.transform.position;
+            player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+        /*
+		for (int i = 0; i < cordLength; i++)
+        {
+            _cordParts[i].transform.position = (this.gameObject.transform.position + ((player.transform.position - this.gameObject.transform.position).normalized * (this.gameObject.transform.position - player.transform.position).magnitude/cordLength*i));
+        }
+        /**/
+
+        for (int i = 0; i < cordLength; i++)
+        {
+            //_cordParts[i].transform.position = new Vector2(-(this.gameObject.transform.position.x - player.transform.position.x) / cordLength * i, -Mathf.Log((this.gameObject.transform.position.y - player.transform.position.y)*i, 2.0f));
+
+            _cordParts[i].transform.position = (this.gameObject.transform.position + ((player.transform.position - this.gameObject.transform.position).normalized * (this.gameObject.transform.position - player.transform.position).magnitude / cordLength * i));
+            _cordParts[i].transform.position = (new Vector3 (_cordParts[i].transform.position.x, _cordParts[i].transform.position.y + (cordFloppiness * Mathf.Sin((_sineLengthModifier * 3.14f) * i + 3.14f))));
+            float x = _cordParts[i].transform.position.x;
+            float y = _cordParts[i].transform.position.y;
+        }
+        /**/
+    }
+}
---
+++ 2D test box/Assets/Scripts/SlimeScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeScript : MonoBehaviour
+{
+    public Transform player;
+
+    public float thinkSpeed = 3.0f;
+    public float detectionRange = 5.0f;
+    public float useMagnitudeRange = 2.0f;
+
+    public float jumpDistance = 1.0f;
+    public float jumpHeight = 1.0f;
+
+    private float _thinkTimer = 3.0f;
+
+    private void jumpAttack(Vector3 pTarget, float pXVelocity = 1.0f, float pYVelocity = 1.0f)
+    {
+        Vector3 normalized = new Vector3(pTarget.x - gameObject.transform.position.x, pTarget.y - gameObject.transform.position.y, 0).normalized;
+        this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(normalized.x * pXVelocity, normalized.y + 1.0f * pYVelocity));
+    }
+
+    private void Step()
+    {
+        float distance = (player.position - gameObject.transform.position).magnitude;
+        Debug.Log(distance);
+
+        if (distance < detectionRange)
+        {
+            if (distance < useMagnitudeRange)
+                jumpAttack(player.position, distance, jumpHeight);
+            else
+                jumpAttack(player.position, jumpDistance, jumpHeight);
+        }
+
+        Debug.Log("called step");
+        _thinkTimer = thinkSpeed;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J)) jumpAttack(player.position, jumpDistance, jumpHeight);
+
+        if (_thinkTimer <= 0) Step();
+        else _thinkTimer -= Time.deltaTime;
+    }
+}

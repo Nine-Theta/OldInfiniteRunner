a3ea1034ced7dafcb18cfa668a2cf71e15897a10
CHRISTIAN\Christian <433910@student.saxion.nl>
2017-12-04 13:31:58 +0100

    Made Parallax Layers

--- 2D test box/Assets/ParallaxScroller.cs
+++ 2D test box/Assets/ParallaxScroller.cs
@@ -3,20 +3,27 @@ using System.Collections;
 
 public class ParallaxScroller : MonoBehaviour
 {
-    public float scrollSpeed;
+    public float scrollSpeed = 0.01f;
     public float tileSizeZ;
 
-    private Vector3 startPosition;
+    private Vector3 _startPosition;
+    private Vector3 _playerStartPosition;
+    private Transform _player;
 
     void Start()
     {
-        startPosition = transform.position;
+        tileSizeZ = Mathf.Abs(tileSizeZ);
+        _startPosition = transform.position;
+        _player = MovementScript.GetPlayer().transform;
+        _playerStartPosition = _player.position;
     }
 
     void Update()
     {
-        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
-        //Debug.Log(Time.time);
-        transform.position = startPosition + Vector3.right * newPosition;
+        float newPosition = (_playerStartPosition.x - _player.position.x) * scrollSpeed; //Mathf.Repeat( Mathf.Abs(_playerStartPosition.x - _player.position.x) * scrollSpeed, tileSizeZ);
+        //if (_player.position.x > _playerStartPosition.x)
+        //    newPosition *= -1;
+        Vector3 addPos = new Vector3(newPosition, 0.0f);
+        transform.position = _startPosition + addPos;
     }
 }

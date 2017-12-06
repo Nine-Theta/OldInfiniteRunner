82233c1e485f7dac51c8122c11b77fc42a622c13
CHRISTIAN\Christian <433910@student.saxion.nl>
2017-12-06 17:47:55 +0100

    Added KeepInMenuSceneScript to the Assets folder for Scene-keeping purposes.

---
+++ 2D test box/Assets/KeepInMenuScenesScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeepInMenuScenesScript : MonoBehaviour
+{
+
+    
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.activeSceneChanged += OnSceneChange;
+    }
+
+    private void OnSceneChange(Scene arg0, Scene arg1)
+    {
+        if (arg1.name != "Lab" && arg1.name != "Menu2 (1)")
+            Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+
+    }
+}

fb44a232233dd706836ea73797c1172c59ee5649
David <davidheetebrij@gmail.com>
2017-12-06 13:53:35 +0100

    buildsettings & buildsettings accesoires

---
+++ 2D test box/Assets/Scripts/PauseScreenScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseScreenScript : MonoBehaviour
+{
+    [SerializeField] private GameObject _pausePrefab;
+
+    public void LoadScene(string pScene)
+    {
+        SceneManager.LoadScene(pScene);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+        }
+    }
+}

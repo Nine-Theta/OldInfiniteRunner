bf7fb9a4678d3377529c01cc1c286aba5323bf2e
CHRISTIAN\Christian <433910@student.saxion.nl>
2017-12-06 10:39:23 +0100

    Added animation delay script

---
+++ 2D test box/Assets/AnimationDelay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationDelay : MonoBehaviour
+{
+    [SerializeField]
+    private float delayInSeconds = 1.0f;
+    [SerializeField]
+    private bool randomDelay = false;
+
+    private Animator _animator;
+
+    void Start()
+    {
+        if (randomDelay)
+            delayInSeconds = Random.Range(0.0f, delayInSeconds);
+        _animator = GetComponent<Animator>();
+        _animator.enabled = false;
+    }
+
+    void Update()
+    {
+        delayInSeconds -= Time.deltaTime;
+        if(delayInSeconds <= 0.0f)
+        {
+            _animator.enabled = true;
+            Destroy(this);
+        }
+    }
+}
